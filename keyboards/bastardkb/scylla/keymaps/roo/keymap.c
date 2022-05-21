74aed4acf1cc175d8a94e0cdfef984b07218760e
Roger Volden <rvolden@ucsc.edu>
2022-05-21 02:37:17 -0700

    Update keymap.c

--- keyboards/bastardkb/scylla/keymaps/roo/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/roo/keymap.c
@@ -26,23 +26,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //-------------------------------------------------//-----------------------------------------------------------//
     KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,	                KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
 //-------------------------------------------------//-----------------------------------------------------------//
-    KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_TILDE,
+    KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_TILD,
 //-------------------------------------------------//-----------------------------------------------------------//
                         KC_LGUI, KC_ENT, MO(1),		     MO(2),  KC_SPC, KC_EQL,
-                                 KC_LALT,  KC_LBRC,      KC_RBRC, KC_MINUS
+                                 KC_LALT,  KC_LCBR,      KC_RCBR, KC_MINS
   ),
 
   [1] = LAYOUT_split_4x6_5(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,			  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,
 //---------------------------------------------------------//-----------------------------------------------------------//
-    RESET,   _______, _______, _______, _______, KC_LBRC,			  KC_RBRC, KC_P7,   KC_P8,   KC_P9,   _______,  KC_PLUS,
+    KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,			            KC_F7, KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,
 //---------------------------------------------------------//-----------------------------------------------------------//
-    _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_LPRN,	      KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MINS,  KC_PIPE,
+    KC_LSFT, _______, _______, KC_END, KC_HOME,  KC_GRAVE,	      KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_EQL,  KC_TILD,
 //---------------------------------------------------------//-----------------------------------------------------------//
-    _______, _______, _______, _______, _______, _______,       _______, KC_P1,   KC_P2,   KC_P3,   KC_EQL,   KC_UNDS,
+    KC_LCTL, _______, _______, KC_PGDN, KC_PGUP, _______,       _______, KC_P1,   KC_P2,   KC_P3,   KC_PLUS,   KC_PIPE,
 //---------------------------------------------------------//-----------------------------------------------------------//
-                               KC_LCTL, KC_HOME, KC_TRNS,		    KC_TRNS, KC_RALT, KC_RGUI,
-                                        KC_SPC,  KC_BSPC,       KC_RCTL, KC_ENT
+                               KC_LGUI, KC_ENT, KC_TRNS,		    KC_TRNS, KC_SPC, KC_TRNS,
+                                        KC_LALT,  KC_TRNS,       KC_TRNS, KC_TRNS
   ),
 
   [2] = LAYOUT_split_4x6_5(
