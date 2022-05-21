d3536e83c2ed1dda259d2a801cf9e55d30ebbecf
Roger Volden <rvolden@ucsc.edu>
2022-05-21 02:30:16 -0700

    Update keymap.c

--- keyboards/bastardkb/scylla/keymaps/roo/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/roo/keymap.c
@@ -22,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_4x6_5(
     KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5,			            KC_6, KC_7, KC_8,    KC_9,   KC_0,     KC_BSPC,
 //-------------------------------------------------//-----------------------------------------------------------//
-    KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,			            KC_Y, KC_U, KC_I,    KC_O,   KC_P,     KC_MINS,
+    KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,			            KC_Y, KC_U, KC_I,    KC_O,   KC_P,     KC_BSLS,
 //-------------------------------------------------//-----------------------------------------------------------//
     KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,	                KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
 //-------------------------------------------------//-----------------------------------------------------------//
-    KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_BSLS,
+    KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_TILDE,
 //-------------------------------------------------//-----------------------------------------------------------//
-                        KC_LCTL, KC_SPC, MO(1),		     MO(2),  KC_ENT, KC_RGUI,
-                                 KC_HOME,  KC_BSPC,      KC_DEL, KC_RALT
+                        KC_LGUI, KC_ENT, MO(1),		     MO(2),  KC_SPC, KC_EQL,
+                                 KC_LALT,  KC_LBRC,      KC_RBRC, KC_MINUS
   ),
 
   [1] = LAYOUT_split_4x6_5(
@@ -46,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT_split_4x6_5(
-    KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,			    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,
+    KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,			    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_MINUS,   KC_DEL,
 //---------------------------------------------------------//--------------------------------------------------------------//
-    _______, _______, RGB_RMOD, RGB_TOG, RGB_MOD, KC_LBRC,			  KC_RBRC, _______, KC_NLCK, KC_INS,  KC_SLCK,  KC_MUTE,
+    _______, _______, RGB_RMOD, RGB_TOG, RGB_MOD, KC_LBRC,			  KC_RBRC, _______, KC_UP, KC_INS,  KC_PSCR,  KC_MUTE,
 //---------------------------------------------------------//--------------------------------------------------------------//
-    _______, KC_LEFT, KC_UP,    KC_DOWN, KC_RGHT, KC_LPRN,	      KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, _______,  KC_VOLU,
+    _______, KC_LEFT, KC_UP,    KC_DOWN, KC_RGHT, KC_LPRN,	      KC_RPRN, KC_LEFT, KC_DOWN, KC_RIGHT, _______,  KC_VOLU,
 //---------------------------------------------------------//--------------------------------------------------------------//
     _______, _______, _______,  _______, _______, _______,        _______, _______, _______, _______, _______,  KC_VOLD,
 //---------------------------------------------------------//--------------------------------------------------------------//
